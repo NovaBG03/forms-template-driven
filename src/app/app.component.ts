6f5317c43451f53cbe77ad3e8f06ca8ab8de22ec
Nikita Koynov <nikicha03@gmail.com>
2020-08-24 19:04:33 +0300

    pathing form values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,31 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f', {static: false}) form: NgForm;
+  @ViewChild('f', {static: false}) registerForm: NgForm;
   defaultQuestion = 'teacher';
   answer: string;
   genders: string[] = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.registerForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: ''
+    // });
+
+    this.registerForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   onSubmit() {
-    console.log(this.form);
+    console.log(this.registerForm);
   }
 }
