1df802030560367c192c0184e8a21078d8f959e1
Nikita Koynov <nikicha03@gmail.com>
2020-08-24 19:20:36 +0300

    using form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer: string;
   genders: string[] = ['male', 'female'];
+  isSubmitted = false;
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -32,6 +40,11 @@ export class AppComponent {
   }
 
   onSubmit() {
-    console.log(this.registerForm);
+    this.isSubmitted = true;
+    this.user.username = this.registerForm.value.userData.username;
+    this.user.email = this.registerForm.value.userData.email;
+    this.user.secretQuestion = this.registerForm.value.secret;
+    this.user.answer = this.registerForm.value.questionAnswer;
+    this.user.gender = this.registerForm.value.gender;
   }
 }
